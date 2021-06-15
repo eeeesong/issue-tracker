bb3442496d1573963278409b6c7161108caa5dad
Seong Young Jeong <tjdduddl10@gmail.com>
2021-06-15 12:11:04 +0900

    fix: 충돌해결 과정 중 생긴 오류 수정

--- FE/src/atoms/atoms.tsx
+++ FE/src/atoms/atoms.tsx
@@ -1,4 +1,4 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 interface IuserinfoAtom {
   profileUrl: string;
--- FE/src/components/LabelList/LabelList.tsx
+++ FE/src/components/LabelList/LabelList.tsx
@@ -10,11 +10,17 @@ import LabelModal from "./LabelModal";
 const LabelList = () => {
   const [isAdding, setAdding] = useState(false);
   const labelCount = useRecoilValue(labelCountSelector);
+  // fetch("http://3.34.122.67/api/labels")
+  //   .then((res) => res.json())
+  //   .then((json) => console.log(json));
+  console.log(localStorage.getItem("token"))
   return (
     <LabelListWrapper>
       <Tabs left={0} type="LABEL" />
       <AddButton text="추가" onClick={() => setAdding(true)} />
-      {isAdding && <LabelModal label={{ id: labelCount + 1, name: "", content: "", color_code: "EFF0F6" }} type="ADD" />}
+      {isAdding && (
+        <LabelModal label={{ id: labelCount + 1, name: "", content: "", color_code: "EFF0F6" }} type="ADD" />
+      )}
       <List isAdding={isAdding} />
     </LabelListWrapper>
   );
